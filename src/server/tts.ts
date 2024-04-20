7b53b363487285f43fefc9154d71ed545ed312c8
Matias <matiasdiezcanseco@gmail.com>
2024-04-19 22:08:22 -0500

    lint fixes

--- src/server/tts.ts
+++ src/server/tts.ts
@@ -2,7 +2,7 @@ import "server-only";
 
 export const getTtsRequestsByUserId = async (id: string) => {
   return [
-    { status: "pending", id: "1", text: "Translation 1", audio: undefined },
+    { status: "pending", id, text: "Translation 1", audio: undefined },
     { status: "finished", id: "2", text: "Translation 1", audio: "audio.mp3" },
   ];
 };
